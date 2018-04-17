6b34fc859df9ce96d0ca710235642ea5da12fff8
Nathan Fleurisma <nfleurisma@gmail.com>
2018-04-17 13:55:45 -0400

    final blog

--- app.js
+++ app.js
@@ -1,139 +1,125 @@
 window.addEventListener('load', event => {
-  // Variables
-  const baseURL = 'http://localhost:3000';
-  const allTitles = document.querySelector('.all-titles');
-  const mainContent = document.querySelector('.main')
-  const postActions = document.createElement('div')
-  postActions.classList.add('.post-actions')
-  postActions.innerHTML = `
-  <button class="btn btn-primary edit">Edit</button>
-  <button class="btn btn-danger delete">Delete</button>
-  `
-  let postId;
 
-  //New Post Button
-  const newPostBtnDiv = document.querySelector('.new-post-button');
-  const newPostBtn = document.createElement('button')
-  newPostBtn.classList.add('btn', 'btn-success', 'new-post-btn');
-  newPostBtn.innerHTML = "Create New Post";
-  newPostBtnDiv.appendChild(newPostBtn);
+  console.log('Ajax Blog Client: ready to AJAXX!');
 
-  // New post form
-  const newPostForm = () => {
-    mainContent.innerHTML = '';
-    const blankForm = document.createElement('form')
-    blankForm.innerHTML = `
-      <div class="form-group">
-        <label>Title:</label><br>
-        <input type="text" id="postTitle"><br>
-        <label>Body:</label><br>
-        <textarea type="text" id="postContent"></textarea>
-      </div>
-      <button class="btn btn-primary" id="submit-post">Submit</button>
-      `
-    mainContent.appendChild(blankForm);
-    const submitPostBtn = document.querySelector("#submit-post");
-    submitPostBtn.addEventListener('click', (event) => {
-      let postTitle = document.querySelector("#postTitle").value
-      let postContent = document.querySelector("#postContent").value
-      const postData = {
-        title: postTitle,
-        content: postContent
-      };
-      submitNewPost(postData, event);
-    });
+  const baseURL = 'http://localhost:3001/api/blogposts/';
+  const newPostButtonEl = document.getElementById('new-post');
+  const allPostsEl = document.getElementById('all-posts');
+  const postsListEl = document.createElement('ul');
+  allPostsEl.appendChild(postsListEl);
+  const focusPostEl = document.getElementById('focus-post');
+
+  const deletePost = id => {
+    axios.delete(`${baseURL}${id}`)
+      .then(result => {
+        focusPostEl.innerHTML = '';
+        setTimeout(getAllPosts, 500);
+      })
+      .catch(error => { console.error(error); });
   }
 
-  newPostBtn.addEventListener('click', newPostForm);
-
-  //Show Posts
-  const showPosts = () => {
-    allTitles.innerHTML = '';
-    const postsListEl = document.createElement('ul')
-    postsListEl.classList.add('list-group')
-    allTitles.appendChild(postsListEl);
-    axios.get(`${baseURL}/posts/`)
-      .then(response => {
-        postsListEl.innerHTML = '';
-        let posts = response.data;
-        posts.forEach(post => {
-          const li = document.createElement("li");
-          li.classList.add('list-group-item');
-          li.innerHTML = `${post.title}`;
-          postsListEl.appendChild(li);
-          li.addEventListener('click', () => {
-            mainContent.innerHTML = '';
-            const p = document.createElement("p");
-            p.innerHTML = `${post.content}`
-            mainContent.appendChild(p)
-            mainContent.appendChild(postActions)
-            const id = post.id;
-            document.querySelector('.delete').addEventListener('click', (event) => {
-              deletePost(id);
-              event.preventDefault();
-            });
-            document.querySelector('.edit').addEventListener('click', (event) => {
-              mainContent.innerHTML = `
-                <div class="form-group">
-                  <label>Title:</label><br>
-                  <input type="text" id="postTitle" value="${post.title}"><br>
-                  <label>Body:</label><br>
-                  <textarea type="text" id="postContent" value="${post.content}"></textarea>
-                </div>
-                <button class="btn btn-primary" id="update-post">Update</button>
-                `
-
-                mainContent.innerHTML = `
-                <div class="form-group">
-                  <label>Title:</label><br>
-                  <input type="text" id="postTitle" value="${post.title}"><br>
-                  <label>Body:</label><br>
-                  <textarea type="text" id="postContent" value="${post.content}"></textarea>
-                </div>
-                <button class="btn btn-primary" id="update-post">Update</button>`
-
-              const updatePostBtn = document.querySelector("#update-post");
-
-              updatePostBtn.addEventListener('click', (event) => {
-                console.log(id)
-                const postData = {
-                  id,
-                  title: document.querySelector("#postTitle").value,
-                  content: document.querySelector("#postContent").value
-                };
-                editPost(id, postData);
-                event.preventDefault();
-              });
-            });
-          });
-        });
+  const updatePost = (event, id) => {
+    const newTitle = document.getElementById('edit-post-title').value;
+    const newContent = document.getElementById('edit-post-content').value;
+    const newData = { title: newTitle, content: newContent };
+    axios.put(`${baseURL}${id}`, newData)
+      .then(result => {
+        let update_id = result.data.id;
+        focusPostEl.innerHTML = '';
+        setTimeout(() => {
+          getAllPosts();
+          getOnePost(update_id);
+        }, 500);
       })
-  }
-  showPosts();
-
-  // Create New Post
-  const submitNewPost = (obj, event) => {
-    axios.post(`${baseURL}/posts`, obj)
-      .then(response => {
-        showPosts();
-      })
+      .catch(error => { console.error(error); });
     event.preventDefault();
   }
 
-  // Delete Post
-  const deletePost = (id, event) => {
-    axios.delete(`${baseURL}/posts/${id}`)
-      .then(response => {
-        showPosts();
-      })
+  const editPost = blogpost => {
+    focusPostEl.innerHTML = '';
+    const editPostFormEl = document.createElement('form');
+    editPostFormEl.innerHTML = `
+      <h4>Edit post.</h4>
+      <label>Title</label>
+      <input type='text' id='edit-post-title' value='${blogpost.title}' />
+      <br><br>
+      <label>Content</label>
+      <textarea id='edit-post-content'>${blogpost.content}</textarea>
+      <br><br>
+      <button id='update-post'>Update.</button>`;
+    focusPostEl.appendChild(editPostFormEl);
+    document.getElementById('update-post').addEventListener('click', (event) => {
+      event.preventDefault();
+      updatePost(event, blogpost.id);
+    });
   }
 
-  // Update Post
-  const editPost = (id, obj) => {
-    axios.put(`${baseURL}/posts/${id}`, obj)
-      .then(response => {
-        showPosts();
-      })
+  const getOnePost = id => {
+    axios.get( `${baseURL}${id}`)
+      .then( response => {
+        focusPostEl.innerHTML = '';
+        const postTitleEl = document.createElement('h3');
+        postTitleEl.innerHTML = response.data.title;
+        const postContentEl = document.createElement('p');
+        postContentEl.innerHTML = response.data.content;
+        focusPostEl.appendChild(postTitleEl);
+        focusPostEl.appendChild(postContentEl);
+        const editButtonEl = document.createElement('button');
+        editButtonEl.innerHTML = 'Edit.';
+        editButtonEl.id = 'edit-post-button';
+        focusPostEl.appendChild(editButtonEl);
+        const deleteButtonEl = document.createElement('button');
+        deleteButtonEl.innerHTML = 'Delete.';
+        deleteButtonEl.id = 'delete-post-button';
+        focusPostEl.appendChild(deleteButtonEl);
+        editButtonEl.addEventListener('click', () => { editPost(response.data); });
+        deleteButtonEl.addEventListener('click', () => { deletePost(response.data.id); });
+       })
+      .catch( error => { console.error(error); });
   }
+
+  const createPost = event => {
+    const title = document.getElementById('new-post-title').value;
+    const content = document.getElementById('new-post-content').value;
+    axios.post(`${baseURL}`, { title, content })
+      .then( response => {
+        getAllPosts();
+        getOnePost(response.data.id);
+      })
+      .catch( error => { console.error( error ); });
+    event.preventDefault();
+  }
+
+  const newPost = () => {
+    const newPostFormEl = document.createElement('form');
+    newPostFormEl.innerHTML = `<h4>New post.</h4>
+      <label>Title</label>
+      <input type='text' id='new-post-title' />
+      <br><br>
+      <label>Content</label>
+      <textarea id='new-post-content'></textarea>
+      <br><br>
+      <button id='create-post'>Submit.</button>`;
+    focusPostEl.innerHTML = '';
+    focusPostEl.appendChild(newPostFormEl);
+    document.getElementById('create-post').addEventListener('click', createPost);
+  }
+
+  const getAllPosts = () => {
+    axios.get( baseURL )
+      .then( response => {
+        postsListEl.innerHTML = '';
+        response.data.forEach( item => {
+          let postListItemEl = document.createElement('li');
+          postListItemEl.innerHTML = item.title;
+          postListItemEl.addEventListener('click', id => { getOnePost(item.id); });
+          postsListEl.appendChild(postListItemEl);
+        })
+      })
+      .catch( error => { console.error(error); });
+  }
+
+  newPostButtonEl.addEventListener('click', newPost);
+  getAllPosts();
 
 });
